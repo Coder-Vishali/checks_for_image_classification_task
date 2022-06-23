196fc3cab37eafa01b081934cc6de6ce7fe098de
Vishali Srinivasan <60731083+Coder-Vishali@users.noreply.github.com>
2022-06-23 11:43:51 +0530

    Create copy_files_for_val_test.py

---
+++ copy_files_for_val_test.py
@@ -0,0 +1,27 @@
+import shutil
+import os
+
+source_label = os.path.join(os.getcwd(), r'labels')
+source_image = os.path.join(os.getcwd(), r'dataset_frames')
+
+dest_train_label = os.path.join(os.getcwd(), r'data\labels\val')
+dest_train_image = os.path.join(os.getcwd(), r'data\images\val')
+
+dest_test_label = os.path.join(os.getcwd(), r'data\labels\test')
+dest_test_image = os.path.join(os.getcwd(), r'data\images\test')
+
+trainlist = os.path.join(os.getcwd(), r'data_splits\vallist_1.txt')
+file1 = open(trainlist, 'r')
+Lines = file1.readlines()
+for line in Lines:
+    line = line.strip("\n")
+    shutil.copy(os.path.join(source_label, line), os.path.join(dest_train_label, line))
+    shutil.copy(os.path.join(source_image, line.replace(".txt",".bmp")), os.path.join(dest_train_image, line.replace(".txt",".bmp")))
+
+trainlist = os.path.join(os.getcwd(), r'data_splits\testlist_1.txt')
+file1 = open(trainlist, 'r')
+Lines = file1.readlines()
+for line in Lines:
+    line = line.strip("\n")
+    shutil.copy(os.path.join(source_label, line), os.path.join(dest_test_label, line))
+    shutil.copy(os.path.join(source_image, line.replace(".txt",".bmp")), os.path.join(dest_test_image, line.replace(".txt",".bmp")))
